79f41b0f5969ed02fc81d7bb9ff16d332c8e0b7d
Giorgi Kotchlamazashvili <georgedot@gmail.com>
2023-05-27 22:03:37 +0200

    fix(fetchPublicKey): extract by prefix not order

--- src/client/fetchPublicKey.js
+++ src/client/fetchPublicKey.js
@@ -1,5 +1,29 @@
 import { fetch } from "./fetch.js";
 
+export const _extractVariables = (js) => {
+  return js
+    .split("\n")
+    .map((line) => line.trim())
+    .filter((line) => line.length)
+    .reduce(
+      (acc, line) => {
+        switch (true) {
+          case line.startsWith("var ee="):
+            acc.exponent = line.slice(8, -2);
+            break;
+          case line.startsWith("var nn="):
+            acc.modulus = line.slice(8, -2);
+            break;
+          case line.startsWith("var seq="):
+            acc.sequence = line.slice(9, -2);
+            break;
+        }
+        return acc;
+      },
+      { exponent: "", modulus: "", sequence: "" }
+    );
+};
+
 export const fetchPublicKey = async (baseUrl) => {
   const res = await fetch(new URL("cgi/getParm", baseUrl).href, {
     method: "POST",
@@ -7,11 +31,12 @@ export const fetchPublicKey = async (baseUrl) => {
   });
 
   const js = await res.text();
-  const [eeLine, nnLine, , seqLine] = js.split("\n").map((s) => s.trim());
+
+  const { exponent, modulus, sequence } = _extractVariables(js);
 
   return {
-    exponent: Buffer.from(eeLine.slice(8, -2), "hex"),
-    modulus: Buffer.from(nnLine.slice(8, -2), "hex"),
-    sequence: Number(seqLine.slice(9, -2)),
+    exponent: Buffer.from(exponent, "hex"),
+    modulus: Buffer.from(modulus, "hex"),
+    sequence: Number(sequence),
   };
 };
