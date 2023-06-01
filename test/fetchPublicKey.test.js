c79cd25dfc734b94bbd14a07019157d3e027ad21
Giorgi Kotchlamazashvili <georgedot@gmail.com>
2023-06-01 15:06:18 +0400

    chore: cleanup tests

--- test/fetchPublicKey.test.js
+++ test/fetchPublicKey.test.js
@@ -3,52 +3,22 @@ import assert from "node:assert";
 import { _extractVariables } from "../src/client/fetchPublicKey.js";
 
 test("cgi/getParm samples where all 3 required fields are present", () => {
+  const nnLine = `var nn="${"A".repeat(128)}";`;
+  const eeLine = 'var ee="010100";';
+  const seqLine = 'var seq="111111111";';
   const samples = [
-    [
-      "var userSetting=1;",
-      'var ee="010100";',
-      'var nn="AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";',
-      'var seq="111111111";',
-      "$.ret=0;",
-    ].join("\n"),
-    [
-      'var ee="010100";',
-      'var nn="AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";',
-      'var seq="111111111";',
-      "$.ret=0;",
-    ].join("\n"),
-    [
-      'var ee="010100";',
-      "var userSetting=1;",
-      'var seq="111111111";',
-      'var nn="AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";',
-      "$.ret=0;",
-    ].join("\n"),
-    [
-      'var ee="010100";',
-      'var seq="111111111";',
-      'var nn="AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";',
-    ].join("\n"),
-    [
-      'var ee="010100";',
-      'var nn="AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";',
-      '',
-      'var seq="111111111";',
-      "$.ret=0;",
-    ].join("\n"),
-    [
-      'var ee="010100";',
-      '',
-      'var nn="AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";',
-      '',
-      'var seq="111111111";',
-      '',
-      '',
-      '',
-      '',
-      '',
-      "$.ret=0;",
-    ].join("\n"),
+    ["var userSetting=1;", eeLine, nnLine, seqLine, "$.ret=0;"],
+    [eeLine, nnLine, seqLine, "$.ret=0;"],
+    [eeLine, "var userSetting=1;", seqLine, nnLine, "$.ret=0;"],
+    [eeLine, seqLine, nnLine],
+    [eeLine, nnLine, "", seqLine, "$.ret=0;"],
+    [eeLine, "", nnLine, "", seqLine, "", "", "", "", "", "$.ret=0;"],
+    ["", eeLine, "", nnLine, "", seqLine, "", "", "", "", "", "$.ret=0;"],
+    ["", "", nnLine, "", eeLine, seqLine, "", "", "", "", "", "$.ret=0;"],
+    [nnLine, eeLine, seqLine],
+    [seqLine, nnLine, eeLine],
+    [eeLine, nnLine, seqLine],
+    [eeLine, seqLine, nnLine],
   ];
 
   const expected = {
@@ -59,6 +29,6 @@ test("cgi/getParm samples where all 3 required fields are present", () => {
   };
 
   for (const sample of samples) {
-    assert.deepStrictEqual(_extractVariables(sample), expected);
+    assert.deepStrictEqual(_extractVariables(sample.join("\n")), expected);
   }
 });
