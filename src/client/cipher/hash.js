32b0bd2100c37bbae9a5963a9af368ae01e04464
Giorgi Kotchlamazashvili <1886698+hertzg@users.noreply.github.com>
2025-12-30 23:02:25 +0100

    feat: migrate to jsr

--- src/client/cipher/hash.js
+++
@@ -1,3 +0,0 @@
-import { createHash } from "node:crypto";
-
-export const hashMd5 = (buffer) => createHash("md5").update(buffer).digest();
--- src/index.js
+++
@@ -1,3 +0,0 @@
-export { ACT } from "./payload.js";
-export * from "./execute.js";
-export * from "./authenticate.js";
